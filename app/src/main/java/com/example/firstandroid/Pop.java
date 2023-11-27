a685c6d7ad68343cbf3d37f9950c5cdb45e10c00
BuffTaco <jiayue.quan@gmail.com>
2023-11-26 19:45:36 -0800

    Initial commit

---
+++ app/src/main/java/com/example/firstandroid/Pop.java
@@ -0,0 +1,33 @@
+package com.example.firstandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.widget.Button;
+
+
+public class Pop extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.popup_window);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int)(width*.8),(int)(height*.7));
+
+        LanguageManager lang = new LanguageManager(this);
+        Button es = findViewById(R.id.btn_es);
+        es.setOnClickListener(view -> {
+            lang.updateResource("es");
+            recreate();
+        });
+
+    }
+}
